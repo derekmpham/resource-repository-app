64b05ebe0e13362b60236a6fbb60b4008dab359d
Jin <jin.di.giordano@gmail.com>
2017-01-05 11:27:21 -0800

    Update User favorites/favoriter association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_many :favorites
-  has_many :resources, through: :favorites
+  has_many :favorites, foreign_key: "favoriter_id"
   has_many :resources, foreign_key: "creator_id"
 end
