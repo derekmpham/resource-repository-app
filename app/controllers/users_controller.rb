ae1637b95cb331841cdc86d50c8962d753211e21
Jin <jin.di.giordano@gmail.com>
2017-01-05 15:26:06 -0800

    Add link from homepage to new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to '/resources'
     else
       redirect_to '/users/new'
     end
