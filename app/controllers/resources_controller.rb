f7542e7e8f67e7596c1a2793fe9b67cfc0447563
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-01-05 15:25:20 -0800

    Teacher can create new resource

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,4 +2,30 @@ class ResourcesController < ApplicationController
   def index
     @resources = Resource.all
   end
+
+  def create
+    @resource = Resource.new(resource_params)
+    respond_to do |format|
+     if @resource.save
+       format.html { redirect_to @resource, notice: 'Resource was successfully created.' }
+       format.json { render :show, status: :created, location: @resource }
+     else
+       format.html { render :new }
+       format.json { render json: @resource.errors, status: :unprocessable_entity }
+     end
+   end
+  end
+
+  def new
+    @resource = Resource.new
+  end
+
+  def show
+    @resource = Resource.find(params[:id])
+  end
+
+  private
+  def resource_params
+    params.require(:resource).permit(:title, :url)
+  end
 end
