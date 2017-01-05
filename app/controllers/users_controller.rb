24435326a6a8b3aa36031ce074a1d13547492dcb
Jin <jin.di.giordano@gmail.com>
2017-01-05 15:16:55 -0800

    Created User actions new and create, and signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,26 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password_hash, :role)
+  end
+
+
 end
