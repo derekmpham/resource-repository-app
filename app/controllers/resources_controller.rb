f2bdac4b3e9f6e6e638cc842786ee6e30e7c9e00
jdraper9 <jamesdraper24@gmail.com>
2017-01-06 12:09:49 -0800

    Allow teacher to create a resource with tags

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,24 +3,14 @@ class ResourcesController < ApplicationController
   include SessionsHelper
 
   def index
-    @resources = Resource.joins(:favorites).group("resources.id").order("count(favorites.resource_id) desc")
+    # @resources = Resource.joins(:favorites).group("resources.id").order("count(favorites.resource_id) desc")
+    @resources = Resource.all
   end
 
   def create
     @resource = Resource.new(resource_params)
     @resource.creator_id = current_user.id
-    # tags
-    if params[:resource][:tags]
-      input_tags = params[:resource][:tags].split(' ')
-      input_tags.each do |input_tag|
-        # create tag if not found in database
-        Tag.create(name: input_tag) if !Tag.find(name: input_tag)
-  
-        #create resource_tag object in db for every tag associated with resource
-        tag_obj = Tag.find(name: input_tag)
-        ResourcetTag.create(resource_id: @resource.id, tag_id: tag_obj.id)
-      end
-    end
+
 
     # Tag.all.each do
     #   @tag = Tag.new(parsed_tag_field)
@@ -28,6 +18,20 @@ class ResourcesController < ApplicationController
 
     respond_to do |format|
      if @resource.save
+        # tags
+        if params[:resource][:tags]
+          input_tags = params[:resource][:tags].split(' ')
+          input_tags.each do |input_tag|
+            input_tag.downcase!
+            # create tag if not found in database
+            Tag.create(name: input_tag) if !Tag.find_by(name: input_tag)
+      
+            #create resource_tag object in db for every tag associated with resource
+            tag_obj = Tag.find_by(name: input_tag)
+            ResourceTag.create(resource_id: @resource.id, tag_id: tag_obj.id)
+          end
+        end
+
        format.html { redirect_to @resource, notice: 'Resource was successfully created.' }
        format.json { render :show, status: :created, location: @resource }
      else
@@ -43,10 +47,12 @@ class ResourcesController < ApplicationController
 
   def show
     @resource = Resource.find(params[:id])
+    @tags = @resource.tags
+    p @tags
   end
 
   private
   def resource_params
-    params.require(:resource).permit(:title, :url, :tags)
+    params.require(:resource).permit(:title, :url)
   end
 end
