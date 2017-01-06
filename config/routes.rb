2144824c8c58102986f349c9588367d8f2511b3e
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-01-05 17:10:09 -0800

    Work in progress--incorporate favorite features

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :resources
-  get '/tags/new'
-  post '/tags'
+  resources :favorites
+  get '/tags/new', to: 'tags#show'
+  post '/tags', to: 'tags#create'
   root 'users#index'
+  get '/favorites/new', to: 'favorites#show'
+  post '/favorites', to: 'favorites#create'
 end
