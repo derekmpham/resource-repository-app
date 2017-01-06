670b5c82929ce3e4ecbf305fe98e46dcda9ee49c
jdraper9 <jamesdraper24@gmail.com>
2017-01-06 11:44:37 -0800

    Work on tags

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -5,6 +5,19 @@ class ResourcesController < ApplicationController
 
   def create
     @resource = Resource.new(resource_params)
+    # tags
+    if params[:resource][:tags]
+      input_tags = params[:resource][:tags].split(' ')
+      input_tags.each do |input_tag|
+        # create tag if not found in database
+        Tag.create(name: input_tag) if !Tag.find(name: input_tag)
+  
+        #create resource_tag object in db for every tag associated with resource
+        tag_obj = Tag.find(name: input_tag)
+        ResourcetTag.create(resource_id: @resource.id, tag_id: tag_obj.id)
+      end
+    end
+
     respond_to do |format|
      if @resource.save
        format.html { redirect_to @resource, notice: 'Resource was successfully created.' }
@@ -26,6 +39,6 @@ class ResourcesController < ApplicationController
 
   private
   def resource_params
-    params.require(:resource).permit(:title, :url)
+    params.require(:resource).permit(:title, :url, :tags)
   end
 end
